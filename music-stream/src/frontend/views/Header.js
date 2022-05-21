e27879969c79e4e58afc411aa17a903e477afc27
Rony <ronyhernandez3314@gmail.com>
2022-05-21 13:13:09 -0400

    spotify properties can be called file upload focus

--- music-stream/src/frontend/views/Header.js
+++ music-stream/src/frontend/views/Header.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from 'react-bootstrap/Nav'
 import { Navbar } from "react-bootstrap";
 import './styles/Header.css'
 import Home from "./Home";
 import { Link } from "react-router-dom";
+import apiClient from "./spotify"
+
 
 function Header (){
-const [image,setImage]=("https://www.clipartmax.com/png/middle/253-2536520_homicide-cleanup-generic-user-icon.png"
+const [image,setImage]=useState("https://www.clipartmax.com/png/middle/253-2536520_homicide-cleanup-generic-user-icon.png"
 
 );
+useEffect(() => {
+  apiClient.get("me").then(response => {setImage(response.data.images[0].url);});
+}, [])
+
  
     return(
       <div className="Nav-boarder">
+        <img src={image} className="profile-image" alt="profile"/>
+        
     <h1>header</h1>
      
      
